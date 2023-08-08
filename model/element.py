6c9eef7f79de5650a03e9495b2e1353b49ddd5a7
McLaughlin-D <mclaughlin.dani@northeastern.edu>
2023-08-08 12:12:36 -0400

    added velocity attribute and update_posn for element class and subclasses.

--- model/element.py
+++ model/element.py
@@ -7,10 +7,11 @@ class Element(ABC):
     Args:
         ABC (ABC): The abstract base class.
     """
-    def __init__(self, pos: tuple, dim: tuple, imgpath: str) -> None:
-        self._pos = pos
+    def __init__(self, posn: tuple, dim: tuple, imgpath: str, vel: tuple) -> None:
+        self._posn = posn
         self._dim = dim
         self._imgpath = imgpath
+        self._vel = vel
 
         @abstractmethod
         def interact(self):
@@ -30,3 +31,17 @@ class Element(ABC):
                 (self._pos[0] <= char.pos[0] + char.dim[0] <= self._pos[0] + self._dim[0]) and
                 (self.pos[1] <= char.pos[1] + char.dim[1] <= self._pos[1] + self._dim[1])
             )
+        
+        def update_posn(self):
+            """Updates the elements position based on its velocity.
+            """
+            self._pos = self._pos[0] + self._vel[0], self._pos[1] + self._vel[1]
+
+        def set_vel(self, x_vel: int, y_vel: int):
+            """Sets the velocity attribute based on the parameters passed.
+
+            Args:
+                x_vel (int): The new x-velocity of the element.
+                y_vel (int): The new y-velocity of the element.
+            """
+            self._vel = x_vel, y_vel
